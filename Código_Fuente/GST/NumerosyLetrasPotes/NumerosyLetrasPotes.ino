797967fa46222417cabe8c0af516cf18a24d7209
Astor <vastorga@yandex.com>
2020-10-29 21:27:27 -0300

    ayudin para Marian

--- Código_Fuente/GST/NumerosyLetrasPotes/NumerosyLetrasPotes.ino
+++ Código_Fuente/GST/NumerosyLetrasPotes/NumerosyLetrasPotes.ino
@@ -8,12 +8,12 @@ int potPin3 = 3;
 //Variable del valor del sensor
 int val1 = 0;
 int val3 = 0;
-int thisByte=0;
+
 //--setup
 void setup() {
   Serial.begin(115200);
 }
-
+int thisByte=0;
 //--loop
 void loop() {
   val1 = analogRead(potPin1); // leo la variable del pote 1
@@ -21,16 +21,16 @@ void loop() {
 
   // convierto con regla de tres simple el valor de 0 a 1024 en A hasta Z 
   //(para esto hay que saber que en ACSII esto es desde 65 hasat 90)
-  thisByte=round(25/1023*val1)+65;
+  thisByte=round((25/1023) * val3) + 65;
   Serial.print("X:"); // de aca en mas ves que uso print sin el ln (line) y lo dejo para el ultimo, fijate lo que pasa en la consola!
   Serial.print(val1);
   Serial.print(", Letra: ");
-  Serial.print(thisByte);
+  Serial.write(thisByte);
   Serial.print(",\tY:");
-  thisByte=round(25/1023*val3)+65;
+  thisByte=round((25/1023) * val3) + 65;
   Serial.print(val3);
   Serial.print(", Letra: ");
-  Serial.print(thisByte);
+   Serial.write(thisByte);
   Serial.println(",");
   delay(50);
 
