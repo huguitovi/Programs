d3f5d064e2738d7fd60f01f8221741cdfa8e615f
Astor <vastorga@yandex.com>
2020-10-29 21:32:21 -0300

    ayudin para Marian

--- Código_Fuente/GST/NumerosyLetrasPotes/NumerosyLetrasPotes.ino
+++ Código_Fuente/GST/NumerosyLetrasPotes/NumerosyLetrasPotes.ino
@@ -14,6 +14,7 @@ void setup() {
   Serial.begin(115200);
 }
 int thisByte=0;
+float f=0;
 //--loop
 void loop() {
   val1 = analogRead(potPin1); // leo la variable del pote 1
@@ -21,16 +22,18 @@ void loop() {
 
   // convierto con regla de tres simple el valor de 0 a 1024 en A hasta Z 
   //(para esto hay que saber que en ACSII esto es desde 65 hasat 90)
-  thisByte=round((25/1023) * val3) + 65;
+  f=(25/1023) * val1; //auxiliar de cuenta para convertir a caracter
+  thisByte=round(f) + 65; // convertir a un valor int en relacion con el caracter
   Serial.print("X:"); // de aca en mas ves que uso print sin el ln (line) y lo dejo para el ultimo, fijate lo que pasa en la consola!
   Serial.print(val1);
   Serial.print(", Letra: ");
   Serial.write(thisByte);
   Serial.print(",\tY:");
-  thisByte=round((25/1023) * val3) + 65;
+  f=(25/1023) * val3;
+  thisByte=round(f) + 65;
   Serial.print(val3);
   Serial.print(", Letra: ");
-   Serial.write(thisByte);
+  Serial.write(thisByte);
   Serial.println(",");
   delay(50);
 
